5ef8af28f8730de0a0c8431f2719d975f55f489a
vlad <vlad.devlikamov@gmail.com>
2019-04-21 14:18:15 +0300

    add test pga with max_weight_matching

--- tests/include/pga_parametric_test.h
+++ tests/include/pga_parametric_test.h
@@ -103,6 +103,36 @@ class pga_washington_test : public testing::TestWithParam<int> {
         }
     }
 
+    void pga_on_csr_max_weight() {
+        CSR<int> csr = washington_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+            return max_weight_matching<int>(graph);
+        });
+        std::vector <int> ed(csr.n, 0);
+        int k = 1;
+        for (int i = 0; i < matching.n; i++) {
+            ed[matching.edge_b[i]] = k;
+            ed[matching.edge_e[i]] = k;
+            ++k;
+        }
+        for (int i = 0; i < matching.n; i++) {
+            int to = matching.edge_b[i],
+            to1 = matching.edge_e[i];
+            for (int j = csr.offset[to]; j < csr.offset[to + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to1) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+            for (int j = csr.offset[to1]; j < csr.offset[to1 + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+        }
+    }
+
     void test_pga_on_csr_random() {
         CSR<int> csr = washington_test<int>(value);
         Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
@@ -137,6 +167,21 @@ class pga_washington_test : public testing::TestWithParam<int> {
         CSR<int> csr = washington_test<int>(value);
         Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
             return edmonds<int>(graph);
+        });
+        std::map <int, int> m;
+        for (int i = 0; i < matching.n; i++) {
+            m[matching.edge_e[i]]++;
+            m[matching.edge_b[i]]++;
+        }
+        for (auto it : m) {
+            EXPECT_EQ(1, it.second);
+        }
+    }
+
+    void test_pga_on_csr_max_weight() {
+        CSR<int> csr = washington_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+           return max_weight_matching<int>(graph);
         });
         std::map <int, int> m;
         for (int i = 0; i < matching.n; i++) {
@@ -248,6 +293,36 @@ class pga_zadeh_test : public testing::TestWithParam<int> {
         }
     }
 
+    void pga_on_csr_max_weight() {
+        CSR<int> csr = zadeh_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+            return max_weight_matching<int>(graph);
+        });
+        std::vector <int> ed(csr.n, 0);
+        int k = 1;
+        for (int i = 0; i < matching.n; i++) {
+            ed[matching.edge_b[i]] = k;
+            ed[matching.edge_e[i]] = k;
+            ++k;
+        }
+        for (int i = 0; i < matching.n; i++) {
+            int to = matching.edge_b[i],
+            to1 = matching.edge_e[i];
+            for (int j = csr.offset[to]; j < csr.offset[to + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to1) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+            for (int j = csr.offset[to1]; j < csr.offset[to1 + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+        }
+    }
+
     void test_pga_on_csr_random() {
         CSR<int> csr = zadeh_test<int>(value);
         Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
@@ -282,6 +357,21 @@ class pga_zadeh_test : public testing::TestWithParam<int> {
         CSR<int> csr = zadeh_test<int>(value);
         Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
             return edmonds<int>(graph);
+        });
+        std::map <int, int> m;
+        for (int i = 0; i < matching.n; i++) {
+            m[matching.edge_e[i]]++;
+            m[matching.edge_b[i]]++;
+        }
+        for (auto it : m) {
+            EXPECT_EQ(1, it.second);
+        }
+    }
+
+    void test_pga_on_csr_max_weight() {
+        CSR<int> csr = zadeh_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+            return max_weight_matching<int>(graph);
         });
         std::map <int, int> m;
         for (int i = 0; i < matching.n; i++) {
@@ -393,6 +483,36 @@ class pga_cube_test : public testing::TestWithParam<int> {
         }
     }
 
+    void pga_on_csr_max_weight() {
+        CSR<int> csr = cube_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+            return max_weight_matching<int>(graph);
+        });
+        std::vector <int> ed(csr.n, 0);
+        int k = 1;
+        for (int i = 0; i < matching.n; i++) {
+            ed[matching.edge_b[i]] = k;
+            ed[matching.edge_e[i]] = k;
+            ++k;
+        }
+        for (int i = 0; i < matching.n; i++) {
+            int to = matching.edge_b[i],
+            to1 = matching.edge_e[i];
+            for (int j = csr.offset[to]; j < csr.offset[to + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to1) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+            for (int j = csr.offset[to1]; j < csr.offset[to1 + 1]; ++j) {
+                int v = csr.edges[j];
+                if (v != to) {
+                    ASSERT_NE(ed[to1], ed[v]);
+                }
+            }
+        }
+    }
+
     void test_pga_on_csr_random() {
         CSR<int> csr = cube_test<int>(value);
         Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
@@ -438,6 +558,21 @@ class pga_cube_test : public testing::TestWithParam<int> {
         }
     }
 
+    void test_pga_on_csr_max_weight() {
+        CSR<int> csr = cube_test<int>(value);
+        Matching matching = PGA(csr, [](const CSR<int>& graph) -> Matching {
+            return max_weight_matching<int>(graph);
+        });
+        std::map <int, int> m;
+        for (int i = 0; i < matching.n; i++) {
+            m[matching.edge_e[i]]++;
+            m[matching.edge_b[i]]++;
+        }
+        for (auto it : m) {
+            EXPECT_EQ(1, it.second);
+        }
+    }
+
  private:
     int value;
 };
@@ -468,15 +603,23 @@ TEST_P(pga_washington_test, csr2) {
 }
 
 TEST_P(pga_washington_test, csr3) {
-    test_pga_on_csr_random();
+    pga_on_csr_max_weight();
 }
 
 TEST_P(pga_washington_test, csr4) {
-    test_pga_on_csr_hard();
+    test_pga_on_csr_random();
 }
 
 TEST_P(pga_washington_test, csr5) {
+    test_pga_on_csr_hard();
+}
+
+TEST_P(pga_washington_test, csr6) {
     test_pga_on_csr_edmonds();
+}
+
+TEST_P(pga_washington_test, csr7) {
+    test_pga_on_csr_max_weight();
 }
 
 TEST_P(pga_zadeh_test, csr) {
@@ -492,27 +635,39 @@ TEST_P(pga_zadeh_test, csr2) {
 }
 
 TEST_P(pga_zadeh_test, csr3) {
-    test_pga_on_csr_random();
+    pga_on_csr_max_weight();
 }
 
 TEST_P(pga_zadeh_test, csr4) {
-    test_pga_on_csr_hard();
+    test_pga_on_csr_random();
 }
 
 TEST_P(pga_zadeh_test, csr5) {
+    test_pga_on_csr_hard();
+}
+
+TEST_P(pga_zadeh_test, csr6) {
     test_pga_on_csr_edmonds();
+}
+
+TEST_P(pga_zadeh_test, csr7) {
+    test_pga_on_csr_max_weight();
 }
 
 TEST_P(pga_cube_test, csr) {
     pga_on_csr_random();
 }
 
-TEST_P(pga_cube_test, csr2) {
+TEST_P(pga_cube_test, csr1) {
     pga_on_csr_hard();
 }
 
-TEST_P(pga_cube_test, csr3) {
+TEST_P(pga_cube_test, csr2) {
     pga_on_csr_edmonds();
+}
+
+TEST_P(pga_cube_test, csr3) {
+    pga_on_csr_max_weight();
 }
 
 TEST_P(pga_cube_test, csr4) {
@@ -525,6 +680,10 @@ TEST_P(pga_cube_test, csr5) {
 
 TEST_P(pga_cube_test, csr6) {
     test_pga_on_csr_edmonds();
+}
+
+TEST_P(pga_cube_test, csr7) {
+    test_pga_on_csr_max_weight();
 }
 
 INSTANTIATE_TEST_SUITE_P(pga_test, pga_washington_test,
